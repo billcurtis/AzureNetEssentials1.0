8d059fa8c5873a9ac2d3c77b68d060fdc5c885ce
Bill Curtis <wcurtis@wcurtis.net>
2018-12-14 08:18:30 -0500

    Console Server improvments

--- DeploymentScripts/ConfigureConsoleServers.ps1
+++ DeploymentScripts/ConfigureConsoleServers.ps1
@@ -9,9 +9,9 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 #Enable Powershell
 set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
 
-#Disable IE-ESC
+<#Disable IE-ESC
 $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
 $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
 Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
-Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 #>
 
