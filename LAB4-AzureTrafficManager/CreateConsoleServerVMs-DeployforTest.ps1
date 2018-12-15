704841926031a21ef1f605a52c80649e525f8344
Bill Curtis <wcurtis@wcurtis.net>
2018-12-15 14:08:02 -0500

    Merged Console templates to get around LOD limitiations

--- LAB4-AzureTrafficManager/CreateConsoleServerVMs-DeployforTest.ps1
+++ LAB4-AzureTrafficManager/CreateConsoleServerVMs-DeployforTest.ps1
@@ -1,10 +1,7 @@
 # parameters 
-$ServergName1 = "NetEssentConsoleServer-East"
-$ServergName2 = "NetEssentConsoleServer-West"
-$templateFile1 = '.\CreateConsoleVM1_Template.json'
-$templateFile2 = '.\CreateConsoleVM2_Template.json'
+$ServergName1 = "NetEssentConsoleServers"
+$templateFile1 = ".\CreateConsoleServers_Template.json"
 $templateparameterFile1 = '.\CreateConsoleServerVM1_Parameters.json'
-$templateparameterFile2 = '.\CreateConsoleServerVM2_Parameters.json'
 
 # import the AzureRM modules
 Import-Module AzureRM.Resources
@@ -14,7 +11,7 @@ Login-AzureRmAccount
 
 # create the resource from the template - pass names as parameters
 New-AzureRmResourceGroup -Location "East US" -Name $ServergName1
-New-AzureRmResourceGroup -Location "West US" -Name $ServergName2
 
 New-AzureRmResourceGroupDeployment -Verbose -Force -ResourceGroupName $ServergName1 -TemplateFile $templateFile1 -TemplateParameterFile $templateparameterFile1 
-New-AzureRmResourceGroupDeployment -Verbose -Force -ResourceGroupName $ServergName2 -TemplateFile $templateFile2 -TemplateParameterFile $templateparameterFile2
+
+#Remove-AzureRmResourceGroup $ServergName1 -Force
