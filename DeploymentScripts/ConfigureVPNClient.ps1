55bd23a087e63fd9aa7daca835e44abea9da107b
Bill Curtis <7672827+billcurtis@users.noreply.github.com>
2023-03-16 08:07:13 -0400

    Update ConfigureVPNClient.ps1
    Fixing bug for the ClientVPN

--- DeploymentScripts/ConfigureVPNClient.ps1
+++ DeploymentScripts/ConfigureVPNClient.ps1
@@ -16,23 +16,34 @@ set-ItemProperty -Path $RDPRegPath -name "fDenyTSConnections" -Value 0
 
 Invoke-Command -ScriptBlock {
 
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Confirm:$false -Force
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Confirm:$false -Force
 
-# Install Chocolatey
+    <# Install Chocolatey
 Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
 
 # Install OpenVPN
 choco install openvpn --params "'/SELECT_SHORTCUTS=1 /SELECT_ASSOCIATIONS=1'" -y
+#>
 
-# Create Lab Cert Files
-New-Item 'C:\Users\All Users\VPNCertificates' -ItemType Directory -Force 
-$p2sClientCertPath = (New-Item 'C:\Users\All Users\Desktop\VPNCertificates\P2SClientCertificate.txt' -ItemType File -Force).FullName
-$p2sClientPrivateKeyPath = (New-Item 'C:\Users\All Users\Desktop\VPNCertificates\P2SClientPrivateKey.txt' -ItemType File -Force).FullName
-$rootCertPath = (New-Item 'C:\Users\All Users\Desktop\VPNCertificates\RootCert.txt' -ItemType File -Force).FullName
+    # Download OpenVPN
+    New-Item -ItemType Directory -Path C:\temp -Force
+    $openVPNuri = 'https://swupdate.openvpn.org/community/releases/OpenVPN-2.5.9-I601-amd64.msi'
+    Invoke-RestMethod -Method Get -Uri $openVPNuri -OutFile 'C:\temp\OpenVPN.msi'
+    Set-Location -Path C:\Temp
+    $expression = "msiexec /i OpenVPN.msi /qb"
+    Invoke-Expression $expression
 
-# Populate Content of Cert Files
-Set-Content -Value ((Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/billcurtis/AzureNetEssentials1.0/master/DeploymentScripts/Artifacts/P2SClientCertificate.txt' -UseBasicParsing).Content) -LiteralPath $p2sClientCertPath -Force
-Set-Content -Value ((Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/billcurtis/AzureNetEssentials1.0/master/DeploymentScripts/Artifacts/P2SClientPrivateKey.txt' -UseBasicParsing).Content) -LiteralPath $p2sClientPrivateKeyPath -Force
-Set-Content -Value ((Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/billcurtis/AzureNetEssentials1.0/master/DeploymentScripts/Artifacts/RootCert.txt' -UseBasicParsing).Content) -LiteralPath $rootCertPath -Force
+
+
+    # Create Lab Cert Files
+    New-Item 'C:\Users\All Users\VPNCertificates' -ItemType Directory -Force 
+    $p2sClientCertPath = (New-Item 'C:\Users\All Users\Desktop\VPNCertificates\P2SClientCertificate.txt' -ItemType File -Force).FullName
+    $p2sClientPrivateKeyPath = (New-Item 'C:\Users\All Users\Desktop\VPNCertificates\P2SClientPrivateKey.txt' -ItemType File -Force).FullName
+    $rootCertPath = (New-Item 'C:\Users\All Users\Desktop\VPNCertificates\RootCert.txt' -ItemType File -Force).FullName
+
+    # Populate Content of Cert Files
+    Set-Content -Value ((Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/billcurtis/AzureNetEssentials1.0/master/DeploymentScripts/Artifacts/P2SClientCertificate.txt' -UseBasicParsing).Content) -LiteralPath $p2sClientCertPath -Force
+    Set-Content -Value ((Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/billcurtis/AzureNetEssentials1.0/master/DeploymentScripts/Artifacts/P2SClientPrivateKey.txt' -UseBasicParsing).Content) -LiteralPath $p2sClientPrivateKeyPath -Force
+    Set-Content -Value ((Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/billcurtis/AzureNetEssentials1.0/master/DeploymentScripts/Artifacts/RootCert.txt' -UseBasicParsing).Content) -LiteralPath $rootCertPath -Force
 
 }
